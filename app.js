180e3571e04d0754d2b8e3f76d03ef15afa7408a
universe_pegasus <krupiej.rita@gmail.com>
2024-09-24 14:35:37 +0100

    Update admin
    Create a secure login

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import router from "./server/routes/main.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./server/config/db.js";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import session from "express-session";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -16,6 +19,18 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+}))
 
 app.use(express.static(__dirname + '/public'));
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,32 @@
 import express from "express";
 import Post from "../model/Post.js";
+import User from "../model/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+
+/**
+ * Cookie Middleware
+ * Check Login
+ */
+const authMiddleware = (req, res, next) => {
+    const token = req.cookie.token;
+    
+    if(!token) return res.status(401).json({ message: "Unauthorized" });
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Unauthorized" })
+    }
+}
 
 /**
  * GET
@@ -30,12 +53,69 @@ router.get('/admin', async (req, res) => {
 router.post('/admin', async (req, res) => {
     try {
         const { username, password } = req.body;
+
+        const user = await User.findOne( { username } );
+
+        if (!user) return res.status(401).json({ message: "Invalid credentials." });
         
-        if (req.body.username === "admin" && req.body.password === "password") {
-            res.send("You are logged in.")
-        } else {
-            res.send("Wrong credentials.")
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(401).json({ message: "Invalid credentials." });
+
+        const token = jwt.sign({ userId: user._id, jwtSecret});
+        res.cookie("token", token, { httpOnly: true });
+        
+        res.redirect("/dashboard");
+
+        res.render('admin/login', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+/**
+ * POST
+ * Admin - Register
+ */
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bycrypt.hash(password, 10);
+
+        try {
+            const user = await User.create ({ username, password: hashedPassword});
+            res.status(201).json({ message: "User Created", user });
+        } catch (error) {
+            if(error.code === 11000) {
+                res.status(409).json({ message: "User already in use" });
+            }
+            res.status(500).json({ message: "Internal server error"});
         }
+        
+        res.render('admin/login', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+/**
+ * GET
+ * Admin Dashboard
+ */
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+    try {
+        const locals = {
+            title: "Dashboard",
+            description: "Admin Dashboard"
+        }
+
+        const data = await Post.countDocuments();
+
+        res.render("admin/dashboard", { locals, data, layout: adminLayout });
+
     } catch (error) {
         console.log(error);
     }
