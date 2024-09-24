460c92904e451cde2be6d595dc8b44a8c996fcc8
universe_pegasus <krupiej.rita@gmail.com>
2024-09-24 13:39:23 +0100

    Create main layout and pages
    Create website pages:
    ◦ home
    ◦ about
    ◦ contact
    
    Create partials:
    ◦ header
    ◦ footer

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,21 @@ import express from "express";
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send("Hello World!");
+
+    const locals = {
+        title: "Notebook",
+        description: "Simple blog created with NodeJS, ExpressJS and MongoDB."
+    }
+
+    res.render("index", { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render("about");
+});
+
+router.get('/contact', (req, res) => {
+    res.render("contact");
 });
 
 export default router;
