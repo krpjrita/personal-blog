b40e4f5afb6560bcd9193544bd5f47b0ec48e0bb
universe_pegasus <krupiej.rita@gmail.com>
2024-09-24 14:05:02 +0100

    Create posts in database
    Start populating the database with posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,46 @@
 import express from "express";
+import Post from "../model/Post.js";
 
 const router = express.Router();
 
-router.get('', (req, res) => {
+/**
+ * GET
+ * HOME
+ */
+router.get('/', async (req, res) => {
 
-    const locals = {
-        title: "Notebook",
-        description: "Simple blog created with NodeJS, ExpressJS and MongoDB."
-    }
+        const locals = {
+            title: "Notebook",
+            description: "Notebook Blog",
+        }
 
-    res.render("index", { locals });
+        try {
+            const data = await Post.find();
+            res.render('index', { locals, data });
+        } catch (error) {
+            console.log(error);
+        }
 });
+
+function insertPostData() {
+    Post.insertMany([
+        {
+            title: 'Building a Blog',
+            body: 'This is the body text'
+        },
+        {
+            title: 'LaLaLa',
+            body: 'Body Body'
+        },
+        {
+            title: 'Trititi',
+            body: 'blablabla'
+        },
+    ])
+}
+
+insertPostData();
+
 
 router.get('/about', (req, res) => {
     res.render("about");
